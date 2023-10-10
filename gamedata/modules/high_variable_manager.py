b6915fbeb98144149208ad3921de4ff46c56be26
Carson Ball <carsonjball@gmail.com>
2023-10-09 20:16:42 -0400

    added variable-modification

---
+++ gamedata/modules/high_variable_manager.py
@@ -0,0 +1,5 @@
+class VariableManager:
+    """The class that manages the modification of important variables such as health of actors and points."""
+
+    def __init__(self) -> None:
+        pass
