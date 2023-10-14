900959d1a33580be6468dde3dc4eca53205f9d15
Carson Ball <carsonjball@gmail.com>
2023-10-14 09:30:04 -0400

    added save_manager instance

--- gamedata/modules/high_variable_manager.py
+++ gamedata/modules/high_variable_manager.py
@@ -1,5 +1,9 @@
+from save_manager import SaveManager
+
 class VariableManager:
     """The class that manages the modification of important variables such as health of actors and points."""
 
     def __init__(self) -> None:
-        pass
+        
+        # Load an instance of SaveManager
+        self.save_manager = SaveManager()
