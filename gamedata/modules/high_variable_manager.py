7f37ac42fd171b764640666d089b6d4b7c7e290e
Carson Ball <carsonjball@gmail.com>
2023-10-14 12:06:38 -0400

    added load and get save_data methods
    added load_save_data, which uses SaveManager to load the save object from the save file, and get_current_save_data, which just returns the current save_data_object instance.

--- gamedata/modules/high_variable_manager.py
+++ gamedata/modules/high_variable_manager.py
@@ -7,3 +7,14 @@ class VariableManager:
         
         # Load an instance of SaveManager
         self.save_manager = SaveManager()
+
+
+    def get_current_save_data(self):
+        """Returns current save object."""
+        return self.save_data_object
+    
+    def load_save_data(self, save_number: int):
+        """Loads the save as current_save_data, and returns it."""
+        self.save_data_object = self.save_manager.load_save(save_number)
+        return self.save_data_object
+
